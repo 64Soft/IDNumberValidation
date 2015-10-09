1efc5b2b586fcd2f801c60a10a8ad7106a9ea91b
Mathieu Souphy <msouphy@gmail.com>
2015-10-09 12:20:48 +0200

    AssemblyInfo

--- src/IDNumberValidation/Properties/AssemblyInfo.cs
+++ src/IDNumberValidation/Properties/AssemblyInfo.cs
@@ -3,3 +3,7 @@
 [assembly: AssemblyTitle("IDNumberValidation")]
 [assembly: AssemblyDescription("Class library providing an interface for ID number validation and aiming to implement as much national validation schemes as possible, for both companies and citizens. Visit the project site if you wish to contribute.")]
 [assembly: AssemblyProduct("IDNumberValidation")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0")]
